6b1468ae9645bd68be726d1c8f817e7134b5b4b8
thitipongr <thitipong.r.psn@gmail.com>
2024-03-19 23:01:16 +0700

    Create function editExistingBook

--- bookManageSystem.js
+++ bookManageSystem.js
@@ -1,4 +1,19 @@
-let bookshelf = [];
+let bookshelf = [
+  {
+    id: 0,
+    name: "mock#1-name",
+    auther: "mock#1-auther",
+    year: "mock#1-year",
+    price: "mock#1-price",
+  },
+  {
+    id: 1,
+    name: "mock#2-name",
+    auther: "mock#2-auther",
+    year: "mock#2-year",
+    price: "mock#2-price",
+  },
+];
 
 const addNewBook = () => {
   const addNewBookHeader = "เพิ่มหนังสือใหม่";
@@ -40,6 +55,63 @@ const viewAllBook = () => {
   main();
 };
 
+const editExistingBook = () => {
+  if (bookshelf.length !== 0) {
+    const bookList = bookshelf.map((book, index) => {
+      return `\n (${index + 1}) รหัส: ${book.id} ชื่อ: ${book.name} โดย: ${
+        book.auther
+      } ของปี: ${book.year} ราตา: ${book.price}`;
+    });
+
+    const bookId = prompt(
+      "กรุณากรอก รหัสหนังสือ ที่ต้องการแก้ไขจากรายการ" + bookList
+    );
+
+    if (bookId !== null) {
+      const fineEditBookIndex = bookshelf.findIndex((book) => {
+        return book.id == bookId;
+      });
+
+      const editBookHeader = "แก้ไขข้อมูลหนังสือ";
+
+      const newName = prompt(
+        editBookHeader +
+          `กรุณากรอก ชื่อหนังสือใหม่ (เดิม: ${bookshelf[fineEditBookIndex].name})`
+      );
+      const newAuther = prompt(
+        editBookHeader +
+          `กรุณากรอก ผู้เขียนใหม่ (เดิม: ${bookshelf[fineEditBookIndex].auther})`
+      );
+      const newYear = prompt(
+        editBookHeader +
+          `กรุณากรอก ปีที่พิมพ์ใหม่ (เดิม: ${bookshelf[fineEditBookIndex].year})`
+      );
+      const newPrice = prompt(
+        editBookHeader +
+          `กรุณากรอก ราคาใหม่ (เดิม: ${bookshelf[fineEditBookIndex].price})`
+      );
+
+      const editConfirm = confirm(
+        `ยืนยันการแก้ไขข้อมูลของหนังสือรหัส: ${bookshelf[fineEditBookIndex].id} โดย\nชื่อหนังสือใหม่: ${newName} (เดิม: ${bookshelf[fineEditBookIndex].name})\n ผู้เขียนใหม่: ${newAuther} (เดิม: ${bookshelf[fineEditBookIndex].auther})\nปีที่พิมพ์ใหม่: ${newYear} (เดิม: ${bookshelf[fineEditBookIndex].year})\nราคาใหม่: ${newPrice} (เดิม: ${bookshelf[fineEditBookIndex].price})`
+      );
+
+      if (editConfirm) {
+        bookshelf[fineEditBookIndex] = {
+          id: fineEditBookIndex,
+          name: newName,
+          auther: newAuther,
+          year: newYear,
+          price: newPrice,
+        };
+      }
+    }
+  } else {
+    alert("ยังไม่มีรายการหนังสือในระบบ");
+  }
+
+  main();
+};
+
 const main = () => {
   const mainOption = prompt(
     `ยินดีต้องรับเข้าสู่ "ระบบจัดการหนังสือ" \nกรุณาเลือกรายการที่ต้องการ โดยกรอก \n1: เพิ่มหนังสือใหม่ \n2: ดูรายการหนังสือทั้งหมด \n3: แก้ไขหนังสือที่มีอยู่ \n4: ลบหนังสือที่มีอยู่`
@@ -52,6 +124,13 @@ const main = () => {
     }
     case "2": {
       viewAllBook();
+      break;
+    }
+    case "3": {
+      editExistingBook();
+      break;
+    }
+    case "00": {
       break;
     }
     default: {
