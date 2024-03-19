62ac149b2c6b73f4659dea7af9ce9ed17485a0f6
thitipongr <thitipong.r.psn@gmail.com>
2024-03-19 15:39:29 +0700

    Create function addNewBook

--- bookManageSystem.js
+++ bookManageSystem.js
@@ -1,17 +1,33 @@
 let bookshelf = [];
 
 const addNewBook = () => {
-  console.log("Run a addNewBook fuction");
+  const addNewBookHeader = "เพิ่มหนังสือใหม่";
+  const bookName = prompt(addNewBookHeader + "\nกรุณากรอก ชื่อหนังสือ");
+  const auther = prompt(addNewBookHeader + "\nกรุณากรอก ผู้เขียน");
+  const year = prompt(addNewBookHeader + "\nกรุณากรอก ปีที่พิมพ์");
+  const price = prompt(addNewBookHeader + "\nกรุณากรอก ราคา");
+
+  const confirmation = confirm(
+    `คุณต้องการเพิ่มหนังสือ \nชื่อ: ${bookName} \nโดย: ${auther} \nของปี: ${year} \nราตา: ${price}`
+  );
+
+  if (confirmation) {
+    const packData = {
+      bookName: bookName,
+      auther: auther,
+      year: year,
+      price: price,
+    };
+
+    bookshelf = [...bookshelf, packData];
+  }
+
+  main();
 };
 
 const main = () => {
   const mainOption = prompt(
-    `ยินดีต้องรับเข้าสู่ "ระบบจัดการหนังสือ" 
-กรุณาเลือกรายการที่ต้องการ โดยกรอก 
-1: เพิ่มหนังสือใหม่ 
-2: ดูรายการหนังสือทั้งหมด
-3: แก้ไขหนังสือที่มีอยู่
-4: ลบหนังสือที่มีอยู่`
+    `ยินดีต้องรับเข้าสู่ "ระบบจัดการหนังสือ" \nกรุณาเลือกรายการที่ต้องการ โดยกรอก \n1: เพิ่มหนังสือใหม่ \n2: ดูรายการหนังสือทั้งหมด \n3: แก้ไขหนังสือที่มีอยู่ \n4: ลบหนังสือที่มีอยู่`
   );
 
   switch (mainOption) {
@@ -20,12 +36,9 @@ const main = () => {
       break;
     }
     default: {
-      alert(`กรุณาเลือกรายการที่ต้องการ โดยกรอก 
-1: เพิ่มหนังสือใหม่ 
-2: ดูรายการหนังสือทั้งหมด
-3: แก้ไขหนังสือที่มีอยู่
-4: ลบหนังสือที่มีอยู่
-เท่านั้น!!`);
+      alert(
+        `กรุณาเลือกรายการที่ต้องการ โดยกรอก \n1: เพิ่มหนังสือใหม่ \n2: ดูรายการหนังสือทั้งหมด \n3: แก้ไขหนังสือที่มีอยู่ \n4: ลบหนังสือที่มีอยู่ \nเท่านั้น!!`
+      );
       main();
     }
   }
